e6bcbb15b2b11d1370c9fb55f62e6221e80a3f3f
RDiGregorio <robertdigreg@gmail.com>
2023-10-24 20:26:59 -0400

    add unit tests

--- test/test.js
+++ test/test.js
@@ -1,3 +1,24 @@
+import {expect} from 'chai';
 import {isJsonPrimitive} from '../src/json.js';
 
-console.log(isJsonPrimitive(0));
+describe('isJsonPrimitive', function () {
+    it('returns true for JSON primitives', function (done) {
+        expect(isJsonPrimitive(null)).to.equal(true);
+        expect(isJsonPrimitive(true)).to.equal(true);
+        expect(isJsonPrimitive(false)).to.equal(true);
+        expect(isJsonPrimitive(0)).to.equal(true);
+        expect(isJsonPrimitive(-1)).to.equal(true);
+        expect(isJsonPrimitive(-1.5)).to.equal(true);
+        expect(isJsonPrimitive("")).to.equal(true);
+        expect(isJsonPrimitive("abc")).to.equal(true);
+        done();
+    });
+
+    it('returns false otherwise', function (done) {
+        expect(isJsonPrimitive(undefined)).to.equal(false);
+        expect(isJsonPrimitive(Infinity)).to.equal(false);
+        expect(isJsonPrimitive({})).to.equal(false);
+        expect(isJsonPrimitive([])).to.equal(false);
+        done();
+    });
+});
