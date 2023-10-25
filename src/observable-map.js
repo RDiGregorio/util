6ffdbaa5f73186fe07fd63eddb1064ad9f8f3780
RDiGregorio <robertdigreg@gmail.com>
2023-10-24 22:31:01 -0400

    update comments

--- src/observable-map.js
+++ src/observable-map.js
@@ -68,7 +68,7 @@ export class ObservableMap extends Map {
     }
 
     /**
-     * Mirrors an event trigger, possibly causing entries to be deleted or updated, and a new event to be dispatched.
+     * Mirrors an event trigger, possibly causing entries to be deleted or updated. Dispatches a new event.
      * @param {{type: string, path: any[], value: any}} event
      */
 
