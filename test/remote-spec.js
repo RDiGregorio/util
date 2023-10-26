af8458de49eb54532dbf26471e687a90764095cb
RDiGregorio <robertdigreg@gmail.com>
2023-10-26 18:01:21 -0400

    remote class

--- test/remote-spec.js
+++ test/remote-spec.js
@@ -1,16 +1,14 @@
 import {expect} from 'chai';
 import {ObservableMap} from '../src/observable-map.js';
 import {createMapReviver, mapReplacer} from '../src/json.js';
-import {remote} from '../src/remote.js';
+import {Remote} from '../src/remote.js';
 
 describe('remote', function () {
     it('can sync local and remote maps', function (done) {
-        const
-            observableMaps = [new ObservableMap(),new ObservableMap()],
-            mapReviver = createMapReviver([ObservableMap]);
+        const observableMaps = [new ObservableMap(), new ObservableMap()], remote = new Remote([ObservableMap]);
 
         observableMaps[0].addEventListener((type, path, value) =>
-            remote(observableMaps[1], JSON.stringify({type: type, path: path, value: value}, mapReplacer), mapReviver)
+            remote.sync(observableMaps[1], Remote.encodeEvent(type, path, value))
         );
 
         observableMaps[0].set('a', new ObservableMap([['b', 0]]));
@@ -18,6 +16,7 @@ describe('remote', function () {
         let result;
         observableMaps[1].addEventListener((type, path, value) => result = [type, path, value]);
         observableMaps[0].get('a').dispatchEvent('c', 1);
+        expect(result).to.eql(['c', ['a'], 1]);
         done();
     });
 });
