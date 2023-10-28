46b114cf053d181f09cb3b9dd4fb342728af0ce1
RDiGregorio <robertdigreg@gmail.com>
2023-10-28 11:21:25 -0400

    server/client

--- src/_server.js
+++ src/_server.js
@@ -2,58 +2,6 @@ import {WebSocketServer} from 'ws';
 import {createMapReviver, mapReplacer} from './json.js';
 import {ObservableMap} from './observable-map.js';
 import {createPromise} from './promise.js';
-
-class Server {
-    #onConnect;
-    #onError = console.error;
-    #onMessage;
-    #server;
-
-    connect(port = 8080) {
-        const [promise, resolve] = createPromise();
-        this.#server = new WebSocketServer({port: port});
-        this.#server.on('error', this.#onError);
-        this.#server.on('wsClientError', this.#onError);
-        this.#server.on('listening', () => resolve());
-        this.#server.on('close', () => resolve());
-
-        this.#server.on('connection', (socket, request) => {
-            socket.on('error', this.#onError);
-
-            function send(message) {
-                console.debug(`sending: ${message}`);
-                socket.send(message);
-            }
-
-            try {
-                const state = this.#onConnect(request, send);
-                socket.on('message', message => this.#onMessage(state, send, `${message}`));
-            } catch (error) {
-                this.#onError(error);
-            }
-        });
-
-        return promise;
-    }
-
-    close() {
-        this.#server.close();
-    }
-
-    onConnect(callback) {
-        this.#onConnect = callback;
-    }
-
-    onError(callback) {
-        this.#onError = callback;
-    }
-
-    onMessage(callback) {
-        this.#onMessage = callback;
-    }
-}
-
-
 /**
  * Creates a new server. Returns a function to close the server.
  * @param {ObservableMap} model
