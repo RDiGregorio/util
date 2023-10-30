9acda294dd84be8ef4ed65157aa8db51814efdcc
RDiGregorio <robertdigreg@gmail.com>
2023-10-30 19:03:39 -0400

    server/client

--- src/session.js
+++ src/session.js
@@ -4,17 +4,19 @@ import {RemoteModel} from './remote-model.js';
 import {RemoteController} from './remote-controller.js';
 
 export class Session {
+    //todo: need to think about the design here.
 
-    // the idea is to tie the model/controller code together
-
-    static server({server, port, model, controller, replacer, reviver}) {
+    static server({server, port, createModel, createController, replacer, reviver}) {
         const messageServer = new MessageServer({server, port, replacer, reviver});
-        RemoteModel.server(messageServer, model);
-        RemoteController.server(messageServer, controller);
-        return messageServer.close;
+        RemoteModel.server(messageServer, createModel);
+        RemoteController.server(messageServer, createController);
+        return messageServer;
     }
 
     static client({host, port, secure, replacer, reviver}) {
         const messageClient = new MessageClient({host, port, secure, replacer, reviver});
+        RemoteModel.client(messageClient);
+        RemoteController.client(messageClient);
+        return messageClient;
     }
 }
